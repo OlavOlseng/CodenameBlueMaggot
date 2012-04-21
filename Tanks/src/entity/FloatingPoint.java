a10d699730856a5f8a59ca18f6780e99c0f2c6d5
Eirik Jakobsen <eirikjak@gmail.com>
2012-04-21 19:53:28 +0200

    glemte en klasse

---
+++ Tanks/src/entity/FloatingPoint.java
@@ -0,0 +1,12 @@
+package entity;
+
+public class FloatingPoint {
+	private double x;
+	private double y;
+	 FloatingPoint(double x,double y){
+		this.x = x;
+		this.y = y;
+	}
+	public double getX(){return x;}
+	public double getY(){return y;}
+}
---
+++ Tanks/src/entity/PixelHitbox.java
@@ -0,0 +1,23 @@
+package entity;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class PixelHitbox implements Iterable<FloatingPoint> {
+
+
+	ArrayList<FloatingPoint> points;
+	public PixelHitbox(){
+		points = new ArrayList<FloatingPoint>();
+	}
+	public void addPoint(FloatingPoint point){
+		points.add(point);
+	}
+	
+	public FloatingPoint getPoint(int index){return points.get(index);}
+	@Override
+	public Iterator<FloatingPoint> iterator() {
+		// TODO Auto-generated method stub
+		return points.iterator();
+	}
+}
