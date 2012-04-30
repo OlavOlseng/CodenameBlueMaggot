4fabe1a4921bce8f5e1f1237688c879b8fff6330
Habitats <mail@habitats.no>
2012-04-30 19:47:02 +0200

    fixed IO errors, make sure you have a "lvl" folder in the same one as our jar, otherwise it wont work for shit :)"

--- Tanks/src/gfx/ResourceManager.java
+++ Tanks/src/gfx/ResourceManager.java
@@ -10,25 +10,25 @@ public class ResourceManager {
 	private static Terrain TERRAIN;
 	private static RGBImage BACKGROUND;
 
-	public static RGBImage TANK1 = new RGBImage("/graphics/Tank2.png");
-	public static RGBImage TANK2 = new RGBImage("/graphics/Tank2.png");
-	public static RGBImage TANK3 = new RGBImage("/graphics/Tank2.png");
-	public static RGBImage TANK4 = new RGBImage("/graphics/Tank2.png");
-	public static RGBImage SHELL = new RGBImage("/graphics/Shell_temp.png");
-	public static RGBImage SCOREBUBBLE = new RGBImage("/graphics/Scorebubble.png");
-	public static RGBImage CROSSHAIR1 = new RGBImage("/graphics/Crosshair.png");
-	public static RGBImage CROSSHAIR2 = new RGBImage("/graphics/Crosshair.png");
-	public static RGBImage CROSSHAIR3 = new RGBImage("/graphics/Crosshair.png");
-	public static RGBImage CROSSHAIR4 = new RGBImage("/graphics/Crosshair.png");
-	public static RGBImage ROCKET = new RGBImage("/graphics/Rocket_sheet.png");
-	public static RGBImage MINE = new RGBImage("/graphics/Mine_sheet.png");
-	public static RGBImage GRENADE = new RGBImage("/graphics/Grenade_temp.png");
-	public static RGBImage PACKAGE = new RGBImage("/graphics/Package.png");
-	public static RGBImage BUBBLEHEARTH = new RGBImage("/graphics/BubbleHearth.png");
-	public static RGBImage AIRSTRIKEBEACON = new RGBImage("/graphics/AirStrikeBeacon.png");
-	public static RGBImage BULLET = new RGBImage("/graphics/Bullet.png");
-	public static RGBImage HEART = new RGBImage("/graphics/Heart.png");
-	public static Color COLORMASK = new Color(0x00FAE1);
+	public static final RGBImage TANK1 = new RGBImage("/graphics/Tank2.png");
+	public static final RGBImage TANK2 = new RGBImage("/graphics/Tank2.png");
+	public static final RGBImage TANK3 = new RGBImage("/graphics/Tank2.png");
+	public static final RGBImage TANK4 = new RGBImage("/graphics/Tank2.png");
+	public static final RGBImage SHELL = new RGBImage("/graphics/Shell_temp.png");
+	public static final RGBImage SCOREBUBBLE = new RGBImage("/graphics/Scorebubble.png");
+	public static final RGBImage CROSSHAIR1 = new RGBImage("/graphics/Crosshair.png");
+	public static final RGBImage CROSSHAIR2 = new RGBImage("/graphics/Crosshair.png");
+	public static final RGBImage CROSSHAIR3 = new RGBImage("/graphics/Crosshair.png");
+	public static final RGBImage CROSSHAIR4 = new RGBImage("/graphics/Crosshair.png");
+	public static final RGBImage ROCKET = new RGBImage("/graphics/Rocket_sheet.png");
+	public static final RGBImage MINE = new RGBImage("/graphics/Mine_sheet.png");
+	public static final RGBImage GRENADE = new RGBImage("/graphics/Grenade_temp.png");
+	public static final RGBImage PACKAGE = new RGBImage("/graphics/Package.png");
+	public static final RGBImage BUBBLEHEARTH = new RGBImage("/graphics/BubbleHearth.png");
+	public static final RGBImage AIRSTRIKEBEACON = new RGBImage("/graphics/AirStrikeBeacon.png");
+	public static final RGBImage BULLET = new RGBImage("/graphics/Bullet.png");
+	public final RGBImage HEART = new RGBImage("/graphics/Heart.png");
+	public static final Color COLORMASK = new Color(0x00FAE1);
 
 	private static ResourceManager instance = null;
 
