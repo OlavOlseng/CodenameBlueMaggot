1c648997c634c149fc19a1f46c1e38cdfc4a41c2
Habitats <mail@habitats.no>
2012-04-30 18:28:58 +0200

    rewritten menus and labels with enum implementation and, changed the ui overlay (added life an increased width, also rewrote the logic in it), added about menu

---
+++ Tanks/src/gfx/Labels.java
@@ -0,0 +1,32 @@
+package gfx;
+
+public enum Labels {
+	RETURN("return"), //
+	APPLY("apply"), //
+	NEW_LAN_GAME("newLanGame"), //
+	NEW_GAME("newGame"), //
+	EXIT("exit"), //
+	LVL_SELECT("lvls"), //
+	CREATORS2("creators2"), //
+	CREATORS("creators"), //
+	PLAYER_ONE("playerOne"), //
+	PLAYER_TWO("playerTwo"), //
+	LOGO("codenameBlueMaggot"), //
+	LAN_NICK("lanNick"), //
+	CONNECT_IP("connectIp"), //
+	IS_HOST("isHost"), //
+	START_GAME("startGame"), //
+	START_LAN_GAME("startLanGame"), //
+	OPTIONS("options"), //
+	ABOUT("about");
+
+	private String fileName;
+
+	Labels(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String toString() {
+		return fileName;
+	}
+}
