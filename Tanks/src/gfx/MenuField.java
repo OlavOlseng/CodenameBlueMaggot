cb3aff057452cd97855d2f10754a29130ab6a045
Olav <olseng.o@gmail.com>
2012-05-01 16:29:59 +0200

    fixed rebinding

--- Tanks/src/gfx/MenuField.java
+++ Tanks/src/gfx/MenuField.java
@@ -52,7 +52,7 @@ public class MenuField extends TextField {
 					setBackground(Menu.green);
 				} else if (type == FieldType.KEY && tmpMsg.length() == 1) {
 					System.out.println(KeyEvent.getKeyText(tmpMsg.charAt(0)));
-					//action.setBind(KeyEvent.getExtendedKeyCodeForChar(tmpMsg.charAt(0)));
+					action.setBind((int)tmpMsg.toUpperCase().charAt(0));
 					setText(tmpMsg);
 					setBackground(Menu.green);
 					return;
